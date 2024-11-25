fe490de94bbd306f7717f46cf352dd12dd79849e
Дмитрий Ельцов <demonmaycry@mail.ru>
2024-11-25 10:40:43 +0300

    Task fixed

--- HW1_MainJavaClassesAndPackages/src/main/java/Child.java
+++ HW1_MainJavaClassesAndPackages/src/main/java/Child.java
@@ -1,4 +1,16 @@
 public class Child extends Parent {
+    static {
+        System.out.println("Child:static 1");
+    }
+
+    {
+        System.out.println("Child:instance 1");
+    }
+
+    static {
+        System.out.println("Child:static 2");
+    }
+
     public Child() {
         System.out.println("Child:constructor");
     }
@@ -8,19 +20,7 @@ public class Child extends Parent {
         System.out.println("Child:name-constructor");
     }
 
-    public static void getChildStaticOne() {
-        System.out.println("Child:static 1");
-    }
-
-    public static void getChildStaticTwo() {
-        System.out.println("Child:static 2");
-    }
-
-    public void getChildInstanceOne() {
-        System.out.println("Child:instance 1");
-    }
-
-    public void getChildInstanceTwo() {
+    {
         System.out.println("Child:instance 2");
     }
 }
--- HW1_MainJavaClassesAndPackages/src/main/java/Main.java
+++ HW1_MainJavaClassesAndPackages/src/main/java/Main.java
@@ -2,16 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("<---Child no constructor--->");
         Child childNoConstructor = new Child();
-        childNoConstructor.getChildInstanceOne();
-        childNoConstructor.getChildInstanceTwo();
-        childNoConstructor.getParentInstanceOne();
-        childNoConstructor.getParentInstanceTwo();
 
         System.out.println("<---Child name constructor--->");
         Child childNameConstructor = new Child("Ivan");
-        childNameConstructor.getChildInstanceOne();
-        childNameConstructor.getChildInstanceTwo();
-        childNameConstructor.getParentInstanceOne();
-        childNameConstructor.getParentInstanceTwo();
     }
 }
