df13b23f2248f5d9950c94272aa89f4d1e6159ba
Дмитрий Ельцов <demonmaycry@mail.ru>
2024-12-08 19:42:20 +0300

    Task is completed

--- HW1_MainJavaClassesAndPackages/src/main/java/ru/sberuniversity/classesandpackages/Main.java
+++ HW1_MainJavaClassesAndPackages/src/main/java/ru/sberuniversity/classesandpackages/Main.java
@@ -2,5 +2,10 @@ package ru.sberuniversity.classesandpackages;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("<---Child no constructor--->");
+        Child childNoConstructor = new Child();
+
+        System.out.println("<---Child name constructor--->");
+        Child childNameConstructor = new Child("Ivan");
     }
 }
