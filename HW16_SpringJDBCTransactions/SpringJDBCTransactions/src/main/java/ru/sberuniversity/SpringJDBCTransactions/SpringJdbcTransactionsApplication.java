5520dc23cb59d4ebd696fb719e360b4cd13ccdcf
Дмитрий Ельцов <demonmaycry@mail.ru>
2025-02-15 13:35:29 +0300

    Initial commit

---
+++ HW16_SpringJDBCTransactions/SpringJDBCTransactions/src/main/java/ru/sberuniversity/SpringJDBCTransactions/SpringJdbcTransactionsApplication.java
@@ -0,0 +1,13 @@
+package ru.sberuniversity.SpringJDBCTransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcTransactionsApplication.class, args);
+	}
+
+}
---
+++ HW16_SpringJDBCTransactions/SpringJDBCTransactions/src/test/java/ru/sberuniversity/SpringJDBCTransactions/SpringJdbcTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sberuniversity.SpringJDBCTransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
